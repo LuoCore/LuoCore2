c4c12eabeb882cb603cce534e59223964ac0136c
LuoCore <luoCore@hotmail.com>
2021-06-21 19:04:47 +0800

    123

--- Web.Layui/Areas/Admin/ViewComponents/SideMenuViewComponent.cs
+++ Web.Layui/Areas/Admin/ViewComponents/SideMenuViewComponent.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Common;
 
 namespace Web.Layui.Areas.Admin.ViewComponents
 {
@@ -19,12 +22,13 @@ namespace Web.Layui.Areas.Admin.ViewComponents
         /// 异步调用
         /// </summary>
         /// <returns></returns>
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            User.
-            //var dd= User.FindFirst(User.Identity.).Value;
-            var dddd= User.Identity.Name;
-            return View();
+            AuthenticateResult authInfo = await HttpContext.AuthenticateAsync(Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme);
+            string roleJson = authInfo.Principal.FindFirstValue(ClaimTypes.Role);
+            List<EntitysModels.Base_Permission> permissionModel= new List<EntitysModels.Base_Permission>();
+            permissionModel = roleJson.ToObject<List<EntitysModels.Base_Permission>>();
+            return View(permissionModel);
         }
     }
 }
---
+++ Web.Layui/Areas/Admin/Views/TagHelpers/PermissionMenuTagHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Layui.Areas.Admin.Views.TagHelpers
+{
+    public class PermissionMenuTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+        }
+
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+
+            StringBuilder output = new StringBuilder();
+            if (_object.ListOfObjects.Count > 0)
+            {
+                output.Append("<ul>");
+
+                foreach (MyObject subItem in _object.listOfObjects)
+                {
+                    output.Append("<li>");
+                    output.Append(_object.Title);
+                    output.Append(html.ShowSubItems(subItem.listOfObjects);
+                    output.Append("</li>")
+                }
+                output.Append("</ul>");
+            }
+           
+
+
+            return base.ProcessAsync(context, output);
+        }
+    }
+}
